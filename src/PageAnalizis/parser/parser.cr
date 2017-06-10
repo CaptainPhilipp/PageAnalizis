6c17ad3afa5c3442a540697e870eb0eb91e4d783
CaptainPhilipp <Ph-s@mail.ru>
2017-06-10 19:02:30 +0300

    draw Parser

---
+++ src/PageAnalizis/parser/parser.cr
@@ -0,0 +1,30 @@
+module PageAnalizis
+  class Parser
+    def self.parse(html : String)
+      new(XML.parse html)
+    end
+
+    def initialize(@node : XML::Node) end
+
+    def attributes
+      @node.attributes
+    end
+
+    def each_children
+      @node.children.each { |child| yield(self.class.new child) }
+    end
+
+    def each_descendant
+      @node.descendant.each { |child| yield(self.class.new child) }
+    end
+
+    def attribute_values(attr : String)
+      attributes = @node.attributes
+      return [] of String unless attributes[attr]?
+
+      attributes[attr].to_s =~ /\A\s?#{attr}="([a-z\s-0-9_]*)"\z/i
+      results = $1.split(" ")
+      results && results.any? ? results : [] of String
+    end
+  end
+end
